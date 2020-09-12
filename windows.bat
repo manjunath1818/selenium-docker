8f2b7ad3c2233232fa9dc1d2253c003073c35014
manjunath1818 <57515636+manjunath1818@users.noreply.github.com>
2020-09-12 08:37:06 +0530

    added windowbatcRemoved

--- windows.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=D:\workspace\ExtentReports
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\Grid.xml
-pause
